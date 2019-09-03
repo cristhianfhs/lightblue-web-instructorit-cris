b985d960d2655557d3c6e9a86dc9afc3072d9be5
Cristhian Fernando Hernandez Salgado <chernandez@instructorit.com>
2019-09-03 16:14:30 +0000

    Changes about the images file.

--- routes/images.js
+++ routes/images.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-var express = require('express');
-var router = express.Router();
-var http = require('request-promise-json');
-var Promise = require('promise');
-var UrlPattern = require('url-pattern');
-var config = require('config');
-var pkgcloud = require('pkgcloud');
-
-//Download the file
-router.get('/:fileName', function(req, res){
-    //res.writeHead(200, {'Content-Type': 'text/html'});
-    //res.write();
-    //res.end();
-});
-
-
-function renderErrorPage(function_input) {
-  var err = function_input.err;
-  var res = function_input.res;
-  res.render('error', {reason: err});
-}
-
-module.exports = router;
-=======
 var express = require('express');
 var router = express.Router();
 var http = require('request-promise-json');
@@ -47,4 +22,3 @@ function renderErrorPage(function_input) {
 }
 
 module.exports = router;
->>>>>>> 1f67a71392d95e0b195716cea562a46369926f73
